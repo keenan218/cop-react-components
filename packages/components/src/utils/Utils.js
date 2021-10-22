b7891a176fed45365a6f76439dc770e7a4232dfe
Paul Graham <paul@smartcasual.io>
2021-10-22 15:49:35 +0100

    Add classBuilder utility function
    Added a `classBuilder` utility function to be used in the components that require a BEM class structure.
    
    Also added a bunch of unit tests for `classBuilder` and the accompanying `toArray` function.

--- packages/components/src/utils/Utils.js
+++ packages/components/src/utils/Utils.js
@@ -1,7 +1,20 @@
-const REGEX = /\${[^{]+}/g;
+const INTERPOLATE_REGEX = /\${[^{]+}/g;
 
 const getObjPath = (path, obj, fallback = '') => {
   return path.split('.').reduce((res, key) => res[key] || fallback, obj);
+};
+
+const concatClasses = (...classes) => {
+  return classes.flat(Infinity).filter(cs => !!cs).join(' ') || undefined;
+};
+
+/**
+ * Create an array from the src parameter.
+ * @param {*} src 
+ * @returns src, if it is already an array; otherwise, an array containing src.
+ */
+export const toArray = (src) => {
+  return Array.isArray(src) ? src : src && [src];
 };
 
 /**
@@ -9,7 +22,7 @@ const getObjPath = (path, obj, fallback = '') => {
  * @param {*} template The template string
  * @param {*} variables The values to be used in the interpolation
  * @param {*} fallback (defaults to '')
- * @returns A fully interpolated string
+ * @returns A fully interpolated string.
  * @example
  *  interpolateString('<span>${item.label}</span>', { item: { id: '1', label: 'Item 1' } });
  *  // <span>Item 1</span>
@@ -20,7 +33,7 @@ const getObjPath = (path, obj, fallback = '') => {
 export const interpolateString = (template, variables, fallback = '') => {
   const obj = variables ?? {};
   if (typeof template === 'string') {
-    return template.replace(REGEX, (match) => {
+    return template.replace(INTERPOLATE_REGEX, (match) => {
       const path = match.slice(2, -1).trim();
       return getObjPath(path, obj, fallback);
     });
@@ -28,8 +41,21 @@ export const interpolateString = (template, variables, fallback = '') => {
   return '';
 };
 
-const Utils = {
-  interpolateString
-};
+/**
+ * Should be used for easy handling of BEM classes.
+ * @param {*} block
+ * @param {*} blockModifiers 
+ * @param {*} blockExtra 
+ * @returns A function that will itself return a string representation of the classes to apply to an element.
+ */
+export const classBuilder = (block, blockModifiers, blockExtra) => {
+  const bModifiers = toArray(blockModifiers);
 
-export default Utils;
+  return (element, elementModifiers, elementExtra) => {
+    const eModifiers = toArray(elementModifiers);
+    if (element) {
+      return concatClasses(`${block}__${element}`, eModifiers?.filter(e => !!e).map(modifier => `${block}__${element}--${modifier}`), elementExtra);
+    }
+    return concatClasses(`${block}`, bModifiers?.filter(b => !!b).map(modifier => `${block}--${modifier}`), blockExtra);
+  };
+}
--- packages/components/src/utils/Utils.test.js
+++ packages/components/src/utils/Utils.test.js
@@ -1,6 +1,142 @@
-import Utils from './Utils';
+import { classBuilder, interpolateString, toArray } from './Utils';
 
 describe('Utils', () => {
+
+  describe('toArray', () => {
+    
+    it('should handle a string input', () => {
+      const INPUT = 'text';
+      const RESULT = toArray(INPUT);
+      expect(RESULT).toHaveLength(1);
+      expect(RESULT[0]).toEqual(INPUT);
+    });
+
+    it('should handle a numeric input', () => {
+      const INPUT = 15;
+      const RESULT = toArray(INPUT);
+      expect(RESULT).toHaveLength(1);
+      expect(RESULT[0]).toEqual(INPUT);
+    });
+
+    it('should handle an object input', () => {
+      const INPUT = { prop: 'value' };
+      const RESULT = toArray(INPUT);
+      expect(RESULT).toHaveLength(1);
+      expect(RESULT[0]).toEqual(INPUT);
+    });
+
+    it('should handle an undefined input', () => {
+      const RESULT = toArray(undefined);
+      expect(RESULT).toBeUndefined();
+    });
+
+    it('should handle no input parameter', () => {
+      const RESULT = toArray();
+      expect(RESULT).toBeUndefined();
+    });
+
+    it('should handle a null input', () => {
+      const RESULT = toArray(null);
+      expect(RESULT).toBeNull();
+    });
+
+    it('should handle an array input', () => {
+      const INPUT = ['a', 'b', 'c'];
+      const RESULT = toArray(INPUT);
+      expect(RESULT).toEqual(INPUT);
+    });
+
+  });
+
+  describe('classBuilder', () => {
+    
+    describe('without block modifiers or extras', () => {
+      const BLOCK = 'block';
+      let cb;
+      beforeEach(() => {
+        cb = classBuilder(BLOCK);
+      });
+
+      it('should correctly return the block', () => {
+        expect(cb()).toEqual(BLOCK);
+      });
+  
+      it('should handle an element parameter', () => {
+        expect(cb('text')).toEqual(`${BLOCK}__text`);
+      });
+  
+      it('should handle an element parameter and a string modifier', () => {
+        expect(cb('thing', 'modifier')).toEqual(`${BLOCK}__thing ${BLOCK}__thing--modifier`);
+      });
+  
+      it('should handle an element parameter and an array of modifiers', () => {
+        expect(cb('thing', ['x', 'y'])).toEqual(`${BLOCK}__thing ${BLOCK}__thing--x ${BLOCK}__thing--y`);
+      });
+  
+      it('should handle an element parameter and an array of modifiers with an extra', () => {
+        expect(cb('thing', ['x', 'y'], 'blob')).toEqual(`${BLOCK}__thing ${BLOCK}__thing--x ${BLOCK}__thing--y blob`);
+      });
+    });
+    
+    describe('with string block modifier', () => {
+      const BLOCK = 'block';
+      const MODIFIER = 'modifier';
+      let cb;
+      beforeEach(() => {
+        cb = classBuilder(BLOCK, MODIFIER);
+      });
+
+      it('should correctly return the block', () => {
+        expect(cb()).toEqual(`${BLOCK} ${BLOCK}--${MODIFIER}`);
+      });
+  
+      it('should still handle an element parameter', () => {
+        expect(cb('text')).toEqual(`${BLOCK}__text`);
+      });
+  
+      it('should still handle an element parameter and a string modifier', () => {
+        expect(cb('thing', 'modifier')).toEqual(`${BLOCK}__thing ${BLOCK}__thing--modifier`);
+      });
+  
+      it('should still handle an element parameter and an array of modifiers', () => {
+        expect(cb('thing', ['x', 'y'])).toEqual(`${BLOCK}__thing ${BLOCK}__thing--x ${BLOCK}__thing--y`);
+      });
+  
+      it('should still handle an element parameter and an array of modifiers with an extra', () => {
+        expect(cb('thing', ['x', 'y'], 'blob')).toEqual(`${BLOCK}__thing ${BLOCK}__thing--x ${BLOCK}__thing--y blob`);
+      });
+    });
+    
+    describe('with array of block modifiers', () => {
+      const BLOCK = 'block';
+      const MODIFIERS = ['alpha', 'bravo', 'charlie'];
+      let cb;
+      beforeEach(() => {
+        cb = classBuilder(BLOCK, MODIFIERS);
+      });
+
+      it('should correctly return the block', () => {
+        const ALL_MODIFIERS = MODIFIERS.map(m => `${BLOCK}--${m}`).join(' ');
+        expect(cb()).toEqual(`${BLOCK} ${ALL_MODIFIERS}`);
+      });
+    });
+    
+    describe('with array of block modifiers and extra', () => {
+      const BLOCK = 'block';
+      const MODIFIERS = ['alpha', 'bravo', 'charlie'];
+      const EXTRA = 'blockExtra'
+      let cb;
+      beforeEach(() => {
+        cb = classBuilder(BLOCK, MODIFIERS, EXTRA);
+      });
+
+      it('should correctly return the block', () => {
+        const ALL_MODIFIERS = MODIFIERS.map(m => `${BLOCK}--${m}`).join(' ');
+        expect(cb()).toEqual(`${BLOCK} ${ALL_MODIFIERS} ${EXTRA}`);
+      });
+    });
+
+  });
 
   describe('interpolateString', () => {
 
@@ -8,7 +144,7 @@ describe('Utils', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<span>${item.label}</span>';
       const OBJECT = { item: { id: '1', label: 'Item 1' } };
-      const RESULT = Utils.interpolateString(TEMPLATE, OBJECT);
+      const RESULT = interpolateString(TEMPLATE, OBJECT);
       expect(RESULT).toEqual('<span>Item 1</span>');
     });
 
@@ -16,7 +152,7 @@ describe('Utils', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
       const OBJECT = { forename: 'Bob', surname: 'Smith' };
-      const RESULT = Utils.interpolateString(TEMPLATE, OBJECT);
+      const RESULT = interpolateString(TEMPLATE, OBJECT);
       expect(RESULT).toEqual('<div>Bob Smith</div>');
     });
 
@@ -25,7 +161,7 @@ describe('Utils', () => {
       const TEMPLATE = '<div>${forename} ${surname}</div>';
       const OBJECT = { forename: 'William' };
       const FALLBACK = 'Fallback-Jones';
-      const RESULT = Utils.interpolateString(TEMPLATE, OBJECT, FALLBACK);
+      const RESULT = interpolateString(TEMPLATE, OBJECT, FALLBACK);
       expect(RESULT).toEqual('<div>William Fallback-Jones</div>');
     });
 
@@ -33,33 +169,33 @@ describe('Utils', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
       const OBJECT = { forename: 'David' };
-      const RESULT = Utils.interpolateString(TEMPLATE, OBJECT);
+      const RESULT = interpolateString(TEMPLATE, OBJECT);
       expect(RESULT).toEqual('<div>David </div>');
     });
 
     it('should handle an undefined template', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const OBJECT = { forename: 'David' };
-      const RESULT = Utils.interpolateString(undefined, OBJECT);
+      const RESULT = interpolateString(undefined, OBJECT);
       expect(RESULT).toEqual('');
     });
 
     it('should handle a null template', () => {
       const OBJECT = { forename: 'David' };
-      const RESULT = Utils.interpolateString(null, OBJECT);
+      const RESULT = interpolateString(null, OBJECT);
       expect(RESULT).toEqual('');
     });
 
     it('should handle a non-string template', () => {
       const OBJECT = { forename: 'David' };
-      const RESULT = Utils.interpolateString(3, OBJECT);
+      const RESULT = interpolateString(3, OBJECT);
       expect(RESULT).toEqual('');
     });
 
     it('should handle an undefined object', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
-      const RESULT = Utils.interpolateString(TEMPLATE, undefined);
+      const RESULT = interpolateString(TEMPLATE, undefined);
       expect(RESULT).toEqual('<div> </div>');
     });
 
@@ -67,14 +203,14 @@ describe('Utils', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
       const FALLBACK = 'Hello';
-      const RESULT = Utils.interpolateString(TEMPLATE, undefined, FALLBACK);
+      const RESULT = interpolateString(TEMPLATE, undefined, FALLBACK);
       expect(RESULT).toEqual('<div>Hello Hello</div>');
     });
 
     it('should handle a null object', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
-      const RESULT = Utils.interpolateString(TEMPLATE, null);
+      const RESULT = interpolateString(TEMPLATE, null);
       expect(RESULT).toEqual('<div> </div>');
     });
 
@@ -82,7 +218,7 @@ describe('Utils', () => {
       // eslint-disable-next-line no-template-curly-in-string
       const TEMPLATE = '<div>${forename} ${surname}</div>';
       const FALLBACK = 'Hello';
-      const RESULT = Utils.interpolateString(TEMPLATE, null, FALLBACK);
+      const RESULT = interpolateString(TEMPLATE, null, FALLBACK);
       expect(RESULT).toEqual('<div>Hello Hello</div>');
     });
 
