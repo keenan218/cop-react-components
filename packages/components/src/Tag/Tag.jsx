f904d78e0df5861b5a2e28bc9bde0cdafdbc1555
Paul Graham <paul@smartcasual.io>
2021-10-25 15:18:45 +0100

    Test Tag component
    Added unit tests and Storybook stories for the `Tag` component, along with an update to the component itself to support full BEM using the `Utils.classBuilder` method.

--- packages/components/src/Tag/Tag.jsx
+++ packages/components/src/Tag/Tag.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { classBuilder } from '../utils/Utils';
 import './Tag.scss';
 
-const Tag = ({ children, text, className }) => {
-  const BASE_CLASS = 'govuk-tag';
-  const classes = () => {
-    return className ? `${BASE_CLASS} ${className}` : BASE_CLASS;
-  };
-  return <strong className={classes()}>
+export const DEFAULT_CLASS = 'govuk-tag';
+const Tag = ({ children, text, classBlock, classModifiers, className, ...attrs }) => {
+  const classes = classBuilder(classBlock, classModifiers, className);
+  return <strong {...attrs} className={classes()}>
     {text || children}
   </strong>;
 };
 
 Tag.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  classBlock: PropTypes.string,
+  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  className: PropTypes.string
+};
+
+Tag.defaultProps = {
+  classBlock: DEFAULT_CLASS
 };
 
 export default Tag;
