df6d095a7270cda64044fd69730c119cf63c267f
Paul Graham <paul@smartcasual.io>
2021-10-26 09:25:51 +0100

    Update Label component
    Updated the Label component to accept BEM attributes, using the new `Utils.classBuilder` method.

--- packages/components/src/Label/Label.jsx
+++ packages/components/src/Label/Label.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { classBuilder } from '../utils/Utils';
 import './Label.scss';
 
 export const OPTIONAL_SUFFIX = ' (optional)';
+export const DEFAULT_CLASS = 'govuk-label';
 const Label = ({
   children,
   id,
   required,
+  classBlock,
+  classModifiers,
+  className,
   ...attrs
 }) => {
-  const [suffix, setSuffix] = useState('');
-  useEffect(() => {
-    if (!required && typeof(children) === 'string') {
-      setSuffix(OPTIONAL_SUFFIX);
-    } else {
-      setSuffix('');
-    }
-  }, [required, children]);
-  return <label {...attrs} htmlFor={id} className="govuk-label">
+  const classes = classBuilder(classBlock, classModifiers, className);
+  return <label {...attrs} htmlFor={id} className={classes()}>
     {children}
-    {suffix}
+    {!required && typeof(children) === 'string' && OPTIONAL_SUFFIX}
   </label>;
 };
 
 Label.propTypes = {
   id: PropTypes.string.isRequired,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  classBlock: PropTypes.string,
+  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  className: PropTypes.string
 };
+
 Label.defaultProps = {
-  required: false
+  required: false,
+  classBlock: DEFAULT_CLASS
 };
+
 export default Label;
