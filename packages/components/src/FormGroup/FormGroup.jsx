eb9f8ec5a6be3b03b25d0a43375235f427a9910a
Paul Graham <paul@smartcasual.io>
2021-10-26 14:39:29 +0100

    Update and test FormGroup component
    Updated the `FormGroup` component to accept BEM attributes, using the `Utils.classBuilder` method.
    
    Also added unit tests and modified the Storybook stories.

--- packages/components/src/FormGroup/FormGroup.jsx
+++ packages/components/src/FormGroup/FormGroup.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { classBuilder, toArray } from '../utils/Utils';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Hint from '../Hint/Hint';
 import Label from '../Label';
 import './FormGroup.scss';
 
+export const DEFAULT_CLASS = 'govuk-form-group';
 const FormGroup = ({
   children,
   id,
   label,
   hint,
   error,
+  required,
+  classBlock,
+  classModifiers: _classModifiers,
   className,
-  required
+  ...attrs
 }) => {
-  const BASE_CLASS = 'govuk-form-group';
-  const classes = () => {
-    let cs = className ? `${className} ${BASE_CLASS}` : BASE_CLASS;
-    if (error) {
-      cs = `${cs} ${BASE_CLASS}--error`;
-    }
-    return cs;
-  };
+  const classModifiers = [...toArray(_classModifiers), error ? 'error' : undefined ];
+  const classes = classBuilder(classBlock, classModifiers, className);
   return (
-    <div key={id} className={classes()}>
+    <div {...attrs} key={id} className={classes()}>
       <Label id={id} required={required}>{label}</Label>
       {hint && <Hint id={id}>{hint}</Hint>}
       {error && <ErrorMessage id={`${id}-error`}>{error}</ErrorMessage>}
@@ -37,9 +36,18 @@ FormGroup.propTypes = {
   label: PropTypes.node.isRequired,
   hint: PropTypes.node,
   error: PropTypes.node,
-  className: PropTypes.string,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  classBlock: PropTypes.string,
+  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  className: PropTypes.string
 };
+
+FormGroup.defaultProps = {
+  required: false,
+  classBlock: DEFAULT_CLASS,
+  classModifiers: []
+};
+
 FormGroup.displayName = 'FormGroup';
 
 export default FormGroup;
