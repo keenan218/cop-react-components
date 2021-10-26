7ed27475f14784758b55596a38138f1623ae850b
Paul Graham <paul@smartcasual.io>
2021-10-26 14:51:10 +0100

    Update and test Details component
    Updated the `Details` component to accept BEM attributes, using the `Utils.classBuilder` method.
    
    Also added some unit tests and Storybook stories.

--- packages/components/src/Details/Details.jsx
+++ packages/components/src/Details/Details.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { classBuilder } from '../utils/Utils';
 import './Details.scss';
 
-const Details = ({
-  children,
-  summary,
-  textClass
-}) => {
-  return <details className="govuk-details">
-    <summary className="govuk-details__summary">{summary}</summary>
-    <div className={ textClass ?? 'govuk-details__text' }>
+export const DEFAULT_CLASS = 'govuk-details';
+const Details = ({ children, summary, classBlock, classModifiers, className, ...attrs }) => {
+  const classes = classBuilder(classBlock, classModifiers, className);
+  return <details {...attrs} className={classes()}>
+    <summary className={classes('summary')}>{summary}</summary>
+    <div className={classes('text')}>
       {children}
     </div>
   </details>;
 };
 
 Details.propTypes = {
-  summary: PropTypes.string.isRequired
+  summary: PropTypes.string.isRequired,
+  classBlock: PropTypes.string,
+  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  className: PropTypes.string
+};
+
+Details.defaultProps = {
+  classBlock: DEFAULT_CLASS
 };
 
 export default Details;
