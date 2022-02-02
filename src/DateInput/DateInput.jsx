0ee631c6850c30e3b685aff4132f62b80e99d40e
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2022-02-02 14:17:55 +0000

    EAB-85 Reuse TextInput component and add Readonly option

--- src/DateInput/DateInput.jsx
+++ src/DateInput/DateInput.jsx
@@ -1,106 +1,96 @@
 // Global Imports
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // Local imports
 import { DEFAULT_CLASS as DEFAULT_LABEL_CLASS } from '../Label/Label';
 import { classBuilder } from '../utils/Utils';
 import './DateInput.scss';
+import TextInput from '../TextInput/TextInput';
+import Readonly from '../Readonly';
 
 export const DEFAULT_CLASS = 'govuk-date-input';
 const DateInput = ({
   id,
-  name,
+  fieldId,
   classBlock,
   classModifiers,
   className,
   error,
-  day,
-  month,
-  year,
+  value,
+  onChange,
+  readonly,
   ...attrs
 }) => {
   const classes = classBuilder(classBlock, classModifiers, className);
 
-  const initialState = {
-    [name + '-day']: day || '',
-    [name + '-month']: month || '',
-    [name + '-year']: year || '',
+  if (readonly) {
+    return (
+      <div id={id} {...attrs}>
+        <div className={classes('item')}>
+          <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-day`}>
+            Day
+          </label>
+          <Readonly id={`${id}-day`}>{value?.day}</Readonly>
+        </div>
+        <div className={classes('item')}>
+          <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-month`}>
+            Month
+          </label>
+          <Readonly id={`${id}-month`}>{value?.month}</Readonly>
+        </div>
+        <div className={classes('item')}>
+          <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-year`}>
+            Year
+          </label>
+          <Readonly id={`${id}-year`}>{value?.year}</Readonly>
+        </div>
+      </div>
+    );
   }
-
-  const [date, setDate] = useState(initialState);
-
-  const onChange = (event) => {
-    setDate({...date, [event.target.name] : event.target.value})
-  };
 
   return (
     <div className={DEFAULT_CLASS} id={id} {...attrs}>
       <div className={classes('item')}>
-        <div className='govuk-form-group'>
-          <label
-            className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`}
-            htmlFor={`${id}-day`}
-          >
-            Day
-          </label>
-          <input
-            className={`govuk-input ${classes('input')} govuk-input--width-2 ${
-              error?.day && 'govuk-input--error'
-            }`}
-            id={`${id}-day`}
-            name={`${name}-day`}
-            type='text'
-            pattern='[0-9]*'
-            inputMode='numeric'
-            value={date[name + '-day']}
-            onChange={onChange}
-          />
-        </div>
+        <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-day`}>
+          Day
+        </label>
+        <TextInput
+          classBlock={`govuk-input ${classes('input')} govuk-input--width-2 ${
+            error?.day && 'govuk-input--error'
+          }`}
+          id={`${id}-day`}
+          fieldId={`${fieldId}-day`}
+          value={value?.day}
+          onChange={onChange}
+        ></TextInput>
       </div>
       <div className={classes('item')}>
-        <div className='govuk-form-group'>
-          <label
-            className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`}
-            htmlFor={`${id}-month`}
-          >
-            Month
-          </label>
-          <input
-            className={`govuk-input ${classes('input')} govuk-input--width-2 ${
-              error?.month && 'govuk-input--error'
-            }`}
-            id={`${id}-month`}
-            name={`${name}-month`}
-            type='text'
-            pattern='[0-9]*'
-            inputMode='numeric'
-            value={date[name + '-month']}
-            onChange={onChange}
-          />
-        </div>
+        <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-month`}>
+          Month
+        </label>
+        <TextInput
+          classBlock={`govuk-input ${classes('input')} govuk-input--width-2 ${
+            error?.month && 'govuk-input--error'
+          }`}
+          id={`${id}-month`}
+          fieldId={`${fieldId}-month`}
+          value={value?.month}
+          onChange={onChange}
+        ></TextInput>
       </div>
       <div className={classes('item')}>
-        <div className='govuk-form-group'>
-          <label
-            className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`}
-            htmlFor={`${id}-year`}
-          >
-            Year
-          </label>
-          <input
-            className={`govuk-input ${classes('input')} govuk-input--width-4 ${
-              error?.year && 'govuk-input--error'
-            }`}
-            id={`${id}-year`}
-            name={`${name}-year`}
-            type='text'
-            pattern='[0-9]*'
-            inputMode='numeric'
-            value={date[name + '-year']}
-            onChange={onChange}
-          />
-        </div>
+        <label className={`${DEFAULT_LABEL_CLASS} ${classes('label')}`} htmlFor={`${id}-year`}>
+          Year
+        </label>
+        <TextInput
+          classBlock={`govuk-input ${classes('input')} govuk-input--width-4 ${
+            error?.year && 'govuk-input--error'
+          }`}
+          id={`${id}-year`}
+          fieldId={`${fieldId}-year`}
+          value={value?.year}
+          onChange={onChange}
+        ></TextInput>
       </div>
     </div>
   );
@@ -108,21 +98,22 @@ const DateInput = ({
 
 DateInput.propTypes = {
   id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  fieldId: PropTypes.string.isRequired,
   classBlock: PropTypes.string,
-  classModifiers: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string),
-  ]),
+  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
   className: PropTypes.string,
   error: PropTypes.shape({
     day: PropTypes.bool,
     month: PropTypes.bool,
     year: PropTypes.bool,
   }),
-  day: PropTypes.number,
-  month: PropTypes.number,
-  year: PropTypes.number,
+  value: PropTypes.shape({
+    day: PropTypes.number,
+    month: PropTypes.number,
+    year: PropTypes.number,
+  }),
+  onChange: PropTypes.func,
+  readonly: PropTypes.bool,
 };
 
 DateInput.defaultProps = {
--- src/DateInput/DateInput.test.js
+++ src/DateInput/DateInput.test.js
@@ -1,6 +1,5 @@
 // Global imports
 import { fireEvent, getByTestId, render } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 // Local imports
 import DateInput, { DEFAULT_CLASS } from './DateInput';
@@ -9,15 +8,140 @@ import { DEFAULT_CLASS as DEFAULT_LABEL_CLASS } from '../Label/Label';
 describe('DateInput', () => {
   const checkSetup = (container, testId) => {
     const wrapper = getByTestId(container, testId);
-    expect(wrapper.classList).toContain(`${DEFAULT_CLASS}`);
     return wrapper;
   };
 
   it('should appropriately set up the necessary components', async () => {
     const ID = 'dateinput';
-    const NAME = 'dateinputId';
+    const FIELD_ID = 'dateinputId';
+    const { container } = render(<DateInput data-testid={ID} id={ID} fieldId={FIELD_ID} />);
+    const wrapper = checkSetup(container, ID);
+    expect(wrapper.classList).toContain(`${DEFAULT_CLASS}`);
+
+    expect(wrapper.childNodes.length).toEqual(3); // 3 for day, month, year
+
+    //day
+    const day = wrapper.childNodes[0];
+    expect(day.classList).toContain(`${DEFAULT_CLASS}__item`);
+
+    const dayLabel = day.childNodes[0];
+    expect(dayLabel.classList).toContain(DEFAULT_LABEL_CLASS);
+    expect(dayLabel.textContent).toEqual('Day');
+
+    const dayInput = day.childNodes[1];
+    expect(dayInput.classList).toContain('govuk-input--width-2');
+    expect(dayInput.id).toEqual(`${ID}-day`);
+    expect(dayInput.name).toEqual(`${FIELD_ID}-day`);
+
+    //month
+    const month = wrapper.childNodes[1];
+    expect(month.classList).toContain(`${DEFAULT_CLASS}__item`);
+
+    const monthLabel = month.childNodes[0];
+    expect(monthLabel.classList).toContain(DEFAULT_LABEL_CLASS);
+    expect(monthLabel.textContent).toEqual('Month');
+
+    const monthInput = month.childNodes[1];
+    expect(monthInput.classList).toContain('govuk-input--width-2');
+    expect(monthInput.id).toEqual(`${ID}-month`);
+    expect(monthInput.name).toEqual(`${FIELD_ID}-month`);
+
+    //year
+    const year = wrapper.childNodes[2];
+    expect(year.classList).toContain(`${DEFAULT_CLASS}__item`);
+
+    const yearLabel = year.childNodes[0];
+    expect(yearLabel.classList).toContain(DEFAULT_LABEL_CLASS);
+    expect(yearLabel.textContent).toEqual('Year');
+
+    const yearInput = year.childNodes[1];
+    expect(yearInput.classList).toContain('govuk-input--width-4');
+    expect(yearInput.id).toEqual(`${ID}-year`);
+    expect(yearInput.name).toEqual(`${FIELD_ID}-year`);
+  });
+
+  it('should show errors with appropriate styling', async () => {
+    const ID = 'dateinput';
+    const FIELD_ID = 'dateinputId';
     const { container } = render(
-      <DateInput data-testid={ID} id={ID} name={NAME} />
+      <DateInput
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        error={{ year: true, month: true, day: true }}
+      />
+    );
+    const wrapper = checkSetup(container, ID);
+    expect(wrapper.classList).toContain(`${DEFAULT_CLASS}`);
+
+    //day
+    const day = wrapper.childNodes[0];
+    const dayInput = day.childNodes[1];
+    expect(dayInput.classList).toContain('govuk-input--error');
+
+    //month
+    const month = wrapper.childNodes[1];
+    const monthInput = month.childNodes[1];
+    expect(monthInput.classList).toContain('govuk-input--error');
+
+    //year
+    const year = wrapper.childNodes[2];
+    const yearInput = year.childNodes[1];
+    expect(yearInput.classList).toContain('govuk-input--error');
+  });
+
+  it('should update values when entered', async () => {
+    const ID = 'dateinput';
+    const FIELD_ID = 'dateinputId';
+
+    let onChangeCalls = 0;
+    const ON_CHANGE = () => {
+      onChangeCalls++;
+    };
+
+    const { container } = render(
+      <DateInput
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        error={{ year: true, month: true, day: true }}
+        value={{ day: 6, month: 3, year: 2076 }}
+        onChange={ON_CHANGE}
+      />
+    );
+    const wrapper = checkSetup(container, ID);
+    expect(wrapper.classList).toContain(`${DEFAULT_CLASS}`);
+
+    //day
+    const dayInput = wrapper.childNodes[0].childNodes[1];
+    expect(dayInput.value).toEqual('6');
+    fireEvent.change(dayInput, { target: { name: FIELD_ID, value: 12 } });
+    expect(onChangeCalls).toEqual(1);
+
+    //month
+    const monthInput = wrapper.childNodes[1].childNodes[1];
+    expect(monthInput.value).toEqual('3');
+    fireEvent.change(monthInput, { target: { value: 2 } });
+    expect(onChangeCalls).toEqual(2);
+
+    //year
+    const yearInput = wrapper.childNodes[2].childNodes[1];
+    expect(yearInput.value).toEqual('2076');
+    fireEvent.change(yearInput, { target: { value: 1999 } });
+    expect(onChangeCalls).toEqual(3);
+  });
+
+  it('should appropriately set up the necessary components when read only', async () => {
+    const ID = 'dateinput';
+    const FIELD_ID = 'dateinputId';
+    const { container } = render(
+      <DateInput
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        value={{ day: 6, month: 3, year: 2076 }}
+        readonly
+      />
     );
     const wrapper = checkSetup(container, ID);
 
@@ -26,104 +150,40 @@ describe('DateInput', () => {
     //day
     const day = wrapper.childNodes[0];
     expect(day.classList).toContain(`${DEFAULT_CLASS}__item`);
-    expect(day.childNodes[0].classList).toContain('govuk-form-group');
 
-    const dayLabel = day.childNodes[0].childNodes[0];
+    const dayLabel = day.childNodes[0];
     expect(dayLabel.classList).toContain(DEFAULT_LABEL_CLASS);
-    expect(dayLabel).toHaveTextContent('Day');
+    expect(dayLabel.textContent).toEqual('Day');
 
-    const dayInput = day.childNodes[0].childNodes[1];
-    expect(dayInput.classList).toContain('govuk-input--width-2');
-    expect(dayInput.id).toEqual(`${ID}-day`);
-    expect(dayInput.name).toEqual(`${NAME}-day`);
+    const dayReadOnly = day.childNodes[1];
+    expect(dayReadOnly.classList).toContain('hods-readonly');
+    expect(dayReadOnly.id).toEqual(`${ID}-day`);
+    expect(dayReadOnly.textContent).toEqual('6');
 
     //month
     const month = wrapper.childNodes[1];
     expect(month.classList).toContain(`${DEFAULT_CLASS}__item`);
-    expect(month.childNodes[0].classList).toContain('govuk-form-group');
 
-    const monthLabel = month.childNodes[0].childNodes[0];
+    const monthLabel = month.childNodes[0];
     expect(monthLabel.classList).toContain(DEFAULT_LABEL_CLASS);
-    expect(monthLabel).toHaveTextContent('Month');
+    expect(monthLabel.textContent).toEqual('Month');
 
-    const monthInput = month.childNodes[0].childNodes[1];
-    expect(monthInput.classList).toContain('govuk-input--width-2');
-    expect(monthInput.id).toEqual(`${ID}-month`);
-    expect(monthInput.name).toEqual(`${NAME}-month`);
+    const monthReadOnly = month.childNodes[1];
+    expect(monthReadOnly.classList).toContain('hods-readonly');
+    expect(monthReadOnly.id).toEqual(`${ID}-month`);
+    expect(monthReadOnly.textContent).toEqual('3');
 
     //year
     const year = wrapper.childNodes[2];
     expect(year.classList).toContain(`${DEFAULT_CLASS}__item`);
-    expect(year.childNodes[0].classList).toContain('govuk-form-group');
 
-    const yearLabel = year.childNodes[0].childNodes[0];
+    const yearLabel = year.childNodes[0];
     expect(yearLabel.classList).toContain(DEFAULT_LABEL_CLASS);
-    expect(yearLabel).toHaveTextContent('Year');
+    expect(yearLabel.textContent).toEqual('Year');
 
-    const yearInput = year.childNodes[0].childNodes[1];
-    expect(yearInput.classList).toContain('govuk-input--width-4');
-    expect(yearInput.id).toEqual(`${ID}-year`);
-    expect(yearInput.name).toEqual(`${NAME}-year`);
-  });
-
-  it('should show errors with appropriate styling', async () => {
-    const ID = 'dateinput';
-    const NAME = 'dateinputId';
-    const { container } = render(
-      <DateInput
-        data-testid={ID}
-        id={ID}
-        name={NAME}
-        error={{ year: true, month: true, day: true }}
-      />
-    );
-    const wrapper = checkSetup(container, ID);
-
-    //day
-    const day = wrapper.childNodes[0];
-    const dayInput = day.childNodes[0].childNodes[1];
-    expect(dayInput.classList).toContain('govuk-input--error');
-
-    //month
-    const month = wrapper.childNodes[1];
-    const monthInput = month.childNodes[0].childNodes[1];
-    expect(monthInput.classList).toContain('govuk-input--error');
-
-    //year
-    const year = wrapper.childNodes[2];
-    const yearInput = year.childNodes[0].childNodes[1];
-    expect(yearInput.classList).toContain('govuk-input--error');
-  });
-
-  it('should update values when entered', async () => {
-    const ID = 'dateinput';
-    const NAME = 'dateinputId';
-    const { container } = render(
-      <DateInput
-        data-testid={ID}
-        id={ID}
-        name={NAME}
-        error={{ year: true, month: true, day: true }}
-      />
-    );
-    const wrapper = checkSetup(container, ID);
-
-    //day
-    const dayInput = wrapper.childNodes[0].childNodes[0].childNodes[1];
-    expect(dayInput.value).toEqual('');
-    fireEvent.change(dayInput, { target: { value: 12 } });
-    expect(dayInput.value).toEqual('12');
-
-    //month
-    const monthInput = wrapper.childNodes[1].childNodes[0].childNodes[1];
-    expect(monthInput.value).toEqual('');
-    fireEvent.change(monthInput, { target: { value: 2 } });
-    expect(monthInput.value).toEqual('2');
-
-    //year
-    const yearInput = wrapper.childNodes[2].childNodes[0].childNodes[1];
-    expect(yearInput.value).toEqual('');
-    fireEvent.change(yearInput, { target: { value: 1999 } });
-    expect(yearInput.value).toEqual('1999');
+    const yearReadOnly = year.childNodes[1];
+    expect(yearReadOnly.classList).toContain('hods-readonly');
+    expect(yearReadOnly.id).toEqual(`${ID}-year`);
+    expect(yearReadOnly.textContent).toEqual('2076');
   });
 });
